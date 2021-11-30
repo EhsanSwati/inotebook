3da12fdff9be6205f75a621ee038aa951fbbe5fa
ehsanullah.khan <ehsanullah.khan@jumpstartpakistan.com>
2021-11-30 14:00:38 +0500

    30-11-21 after video changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -16,52 +16,95 @@ router.post(
     body("Password", "Enter your password Correctly").isLength({ min: 5 }),
     body("Email", "Enter Valid Email address").isEmail(),
     body("Genders", "Enter Your Genders").isLength({ min: 3 }),
-  ],  async (req, res) => {
+  ], async (req, res) => {
     // if there are errors, just return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
     // check whether the user with this email exists already
-    try {   
-    
+    try {
+
       // this part check the email is exist or not 
-    let user = await User.findOne({ Email: req.body.Email });
-    // console.log(user);
-    // if user already exist then this error will be thrown
-    if (user) {
-      return res.status(400).json({ error: "Sorry a user with this email already exists." });
-    }
-    const salt = await bcrypt.genSalt(10);
-    const secPass= await bcrypt.hash(req.body.Password, salt);
-
-    // this is the part where the user is created 
-    user = await User.create({
-      Name: req.body.Name,
-      Password: secPass,
-      Email: req.body.Email,
-      Genders: req.body.Genders,
-    });
-    // here we can pass the response to the server
-    // res.json(user);
-
-    // here we can pass the id to the sarver 
-    const data = {
-      user:{
-        id:user.id
+      let user = await User.findOne({ Email: req.body.Email });
+      // console.log(user);
+      // if user already exist then this error will be thrown
+      if (user) {
+        return res.status(400).json({ error: "Sorry a user with this email already exists." });
       }
-    }
-    const authToken = jwt.sign(data, My_secret);
-    // console.log(authToken);
-    
-    // here we can pass the response to the server
-    res.json({authToken});
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.Password, salt);
 
-  } catch (error) {
+      // this is the part where the user is created 
+      user = await User.create({
+        Name: req.body.Name,
+        Password: secPass,
+        Email: req.body.Email,
+        Genders: req.body.Genders,
+      });
+      // here we can pass the response to the server
+      // res.json(user);
+
+      // here we can pass the id to the sarver 
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, My_secret);
+      // console.log(authToken);
+
+      // here we can pass the response to the server
+      res.json({ authToken });
+
+    } catch (error) {
       console.error(error.message);
       res.status(error.statusCode)
-  }
+    }
   }
 );
+
+
+// Create a user using : POST "/api/auth/Login". No login required
+router.post(
+  "/login",
+  [
+    body("Email", "Enter Valid Email address").isEmail(),
+    body("Password", "Enter your password Correctly").exists(),
+  ], async (req, res) => {
+    // if there are errors, just return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // check whether the user with this email exists already
+    const { Email, Password } = await req.body;
+    try {
+
+      // this part check the email is exist or not 
+      let user = await User.findOne({ Email });
+      // console.log(user);
+      // if user already exist then this error will be thrown
+      if (!user) {
+        return res.status(500).json({ error: "Sorry Wrong Way To Access" });
+      }
+      const comparePasswords = await bcrypt.compare(Password, user.Password)
+      if (!comparePasswords) {
+        return res.status(500).json({ error: "Sorry Wrong Way To Access" });
+      }
+
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, My_secret);
+      res.json({ authToken });
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  });
 
 module.exports = router;
